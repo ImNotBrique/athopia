98262c951cd8689ab1404ff7ee49f47528bce15e
ImNotBrique <52548100+ImNotBrique@users.noreply.github.com>
2019-07-15 14:10:09 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,109 +1,175 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
 
-// This is your client. Some people call it `bot`, some people call it `self`, 
-// some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
-// this is what we're refering to. Your client.
-const client = new Discord.Client();
+const express = require('express');
 
-// Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
-client.login(process.env.TOKEN)
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+const app = express();
 
-client.on("ready", () => {
-    // This event will run if the bot starts, and logs in, successfully.
-    console.log(`Bot lancé, avec ${client.users.size} utilisateurs, dans ${client.channels.size} salons de ${client.guilds.size} serveurs.`); 
-    // Example of changing the bot's playing game to something useful. `client.user` is what the
-    // docs refer to as the "ClientUser".
-    client.user.setActivity(`${client.guilds.size} serveurs | Prefix : +`, { type: 'WATCHING'});
-    client.user.setStatus('dnd')
+const prefix = "+"
+
+const bot = new Discord.Client()    
+
+const talkedRecently = new Set();
+
+bot.login(process.env.TOKEN)
+
+
+
+
+
+bot.on('ready', function() { 
+
+ console.log('Je suis connecté :D') 
+
+
+
+    bot.user.setActivity(`${bot.guilds.size} serveurs | Prefix : +`, { type: 'Listening'});
+
+    bot.user.setStatus('dnd')
+
   });
+
   
-  client.on("guildCreate", guild => {
+
+  bot.on("guildCreate", guild => {
+
     // This event triggers when the bot joins a guild.
+
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`${client.guilds.size} serveurs | Prefix : +`, { type: 'WATCHING'});
-    client.user.setStatus('dnd')
+
+    bot.user.setActivity(`${bot.guilds.size} serveurs | Prefix : +`, { type: 'Listening'});
+
+    bot.user.setStatus('dnd')
+
   });
+
   
-  client.on("guildDelete", guild => {
+
+  bot.on("guildDelete", guild => {
+
     // this event triggers when the bot is removed from a guild.
+
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(` ${client.guilds.size} serveurs | Prefix : +`, { type: 'WATCHING'});
-    client.user.setStatus('dnd')
+
+    bot.user.setActivity(` ${bot.guilds.size} serveurs | Prefix : +`, { type: 'Listening'});
+
+    bot.user.setStatus('dnd')
+
 });
 
-if(command === "kill"){
+bot.on('message', message => {
+  var un_message_est = message
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+ if(command === "kill"){
+      if(talkedRecently.has(message.author.id)) {
+            message.channel.send(":x: **Attends 1 minute avant de refaire cette commande " + message.author.username + " !**");
+    } else {
   let pseudo =  message.mentions.users.first().username
-    if(! message.mentions.members.first()) return message.channel.send('Vous devez mentionner quelqu\'un !')
+    if(!message.mentions.members.first()) return message.channel.send('Vous devez mentionner quelqu\'un !')
 var e = new Discord.RichEmbed()
-.setTitle(`${message.author.username} has kill ${pseudo} :skull_crossbones:`)
+.setTitle(`${message.author.username} a tué ${pseudo} :skull_crossbones:`)
 .setImage("https://cdn.discordapp.com/attachments/580117511941521427/592803283114917910/tenor_1.gif")
 .setColor('000000')
-.setFooter('Killing Commands : +kill @MENTION#0000')
+.setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator')
 message.channel.send(e)
-}
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+          
+          talkedRecently.delete(message.author.id);
+        }, 60000);
+    }
 
-if(command === "ping"){
+}
+  if(command === "ping"){
   
-  message.channel.send('Ping....').then((msg) => {
-    
-    msg.edit(':ping_pong: Pong ! ' + `${Date.now() - message.createdTimestamp}` + " ms !")
+  message.channel.send(new Discord.RichEmbed().addField('Voici le ping du bot !', 'Calcul en cours...').setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator').setColor('RANDOM')).then((msg) => {
+  //`${Date.now() - message.createdTimestamp}`
+    msg.edit(new Discord.RichEmbed().addField('Voici le ping du bot !', ` :ping_pong: Pong ! ${Date.now() - message.createdTimestamp} ms `).setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator').setColor('RANDOM'))
   });
 }
-
-  if(command ===  'clear'){
+  
+   if(command ===  'clear'){
      if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send('Il vous faut la permission `MANAGE_MESSAGES` pour éxécuter cette commande !   Erreur: 009')
     if((!args[0])) return message.channel.send('Merci de spécifier un nombre valide de messages à supprimer !')
     if(args[0] > 100) return message.channel.send('Merci de spécifier un nombre inférieur ou égale à 100')
     
     message.channel.bulkDelete(args[0])
        } 
- 
-if(message.content === prefix + 'serverinfo'){
+ if(message.content === prefix + 'serverinfo'){
     if(!message.guild) return message.channel.send('Une erreur s\'est produite !')
     var e = new Discord.RichEmbed()
     .setThumbnail(message.guild.iconURL)
     .setAuthor(message.guild.name + ' (' + message.guild.id + ')', message.guild.iconURL)
-    .addField('**> Salons**', `> ${message.guild.channels.filter(off => off.type === 'text').size} textuels, ${message.guild.channels.filter(off => off.type === 'voice').size} vocaux\n> Salon afk: ${message.guild.afkChannel}`)
-    .addField('**> Membres**', `> ${message.guild.memberCount} membres (${message.guild.members.filter(o => o.presence.status === 'online').size} :online: ${message.guild.members.filter(member => member.user.bot).size} :clyde:)\n> Owner:${message.guild.owner.user} (${message.guild.ownerID})`)
-    .addField('**> Informations additionelles**',`> Rôles: ${message.guild.roles.size}\n> Région: ${message.guild.region}\n> Date de création: ${message.guild.createdAt}\n> Niveau de vérification: ${message.guild.verificationLevel}`)
-  .setColor('EBDADA')
+    .addField('** :page_facing_up: Salons**', `:arrow_right: ${message.guild.channels.filter(off => off.type === 'text').size} textuels, ${message.guild.channels.filter(off => off.type === 'voice').size} vocaux\n:arrow_right: Salon afk: ${message.guild.afkChannel}`)
+    .addField('** :boy:  Membres**', `:arrow_right: ${message.guild.memberCount} membres (${message.guild.members.filter(o => o.presence.status === 'online').size} en ligne &  ${message.guild.members.filter(member => member.user.bot).size} bot)\n:arrow_right: Owner:${message.guild.owner.user} (${message.guild.ownerID})`)
+    .addField('** :heavy_plus_sign:  Informations additionelles**',`:arrow_right: Rôles: ${message.guild.roles.size}\n:arrow_right: Région: ${message.guild.region}\n:arrow_right: Date de création: ${message.guild.createdAt}\n:arrow_right: Niveau de vérification: ${message.guild.verificationLevel}`)
+    .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator ')
+  .setColor('RANDOM')
     message.channel.send(e)
     }
-
- if(command === 'userinfo'){
+   if(command === 'userinfo'){
   var useruseruser = message.guild.member(message.mentions.users.first());
     var useruseruserauthor = message.mentions.users.first();
     if(!useruseruser) {
       var e = new Discord.RichEmbed()
       .setThumbnail(message.author.displayAvatarURL)
       .setAuthor(message.author.username + '#' + message.author.discriminator + ' (' + message.author.id + ')', message.author.displayAvatarURL)
-      .addField('** Informations de l\'utilisateur**', `> Création du comte: ${message.author.createdAt}\n> Statut: ${message.author.presence.status}\n> Jeu: ${message.author.presence.game}`)
-      .addField('** Informations du membre**', `> Surnom: ${message.member.nickname}\n> A rejoint le serveur : ${message.member.joinedAt}\n> Nombre de rôles: ${message.member.roles.size - 1}`)
+      .addField('** Informations de l\'utilisateur**', `:arrow_right: Création du compte: ${message.author.createdAt}\n:arrow_right: Statut: ${message.author.presence.status}\n:arrow_right: Jeu: ${message.author.presence.game}`)
+      .addField('** Informations du membre**', `:arrow_right: Surnom: ${message.member.nickname}\n:arrow_right: A rejoint le serveur : ${message.member.joinedAt}\n:arrow_right: Nombre de rôles: ${message.member.roles.size - 1}`)
+      .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator')
       .setColor(message.member.displayHexColor)
       message.channel.send(e)
     }else{
       var e = new Discord.RichEmbed()
       .setThumbnail(useruseruserauthor.displayAvatarURL)
       .setAuthor(useruseruserauthor.username + '#' + useruseruserauthor.discriminator + ' (' + useruseruserauthor.id + ')', useruseruserauthor.displayAvatarURL)
-      .addField('**> Informations de l\'utilisateur**', `> Création du comte: ${useruseruserauthor.createdAt}\n> Statut: ${useruseruser.presence.status}\n> Jeu: ${useruseruser.presence.game}`)
-      .addField('**> Informations du membre**', `> Surnom: ${useruseruser.nickname}\n> A rejoint le: ${useruseruser.joinedAt}\n> Nombre de rôles: ${useruseruser.roles.size - 1}`)
-      .setColor(useruseruser.displayHexColor)
+      .addField('** Informations de l\'utilisateur**', `:arrow_right: Création du compte: ${useruseruserauthor.createdAt}\n> Statut: ${useruseruser.presence.status}\n> Jeu: ${useruseruser.presence.game}`)
+      .addField('** Informations du membre**', `:arrow_right: Surnom: ${useruseruser.nickname}\n> A rejoint le: ${useruseruser.joinedAt}\n> Nombre de rôles: ${useruseruser.roles.size - 1}`)
+      .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator')
+      .setColor('RANDOM')
       message.channel.send(e)
     }
     
   }
-
-  if(command === "saying"){
+  
+    if(command === "annonce"){
    var e = new Discord.RichEmbed()
-    .addField(message.author.username, args.join(" "))
+    .addField('Annonce ! ', args.join(" "))
    .setTimestamp()
-   .setColor(message.member.displayHexColor)
-   .setFooter('Message envoyé par ' + message.author.username)
+   .setColor("8B0000")
+   .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator')
+  
    message.channel.send(e)
+   message.channel.send('**[@everyone] Nouvelle Annonce **')
   }
+  
+  
+   
+  if(message.content === prefix + "partenaire"){
+    var e = new Discord.RichEmbed()
+   .addField('Discord des partenaires', '[Serveur Discord](https://discord.gg/PkbhD3f)')    
+      .setColor('RANDOM')   
+    .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator')
+    message.channel.send(e)
+  }
+  
 
-  }}
+   if(message.content === prefix + "help"){ //xd xd
+    if(!message.guild) return message.channel.send('Une erreur s\'est produite !')
+    var e = new Discord.RichEmbed()
+    .setTitle('Help | Aide')
+    .addField('__**Fun**__', `!ping :arrow_right: Ping du BOT`)
+    
+    .setFooter('©️ AthopiaTEAM | Powered by GlitchBot Creator ')
+  .setColor('RANDOM')
+    message.channel.send(e)
+    }
+  
+  
+  // ICI LES COMMANDES DE +WORK ET TOUT MON P'TIT ADAM HéHéHoHoHaHaHuHuHiHi
 });
+
+
+
+client.login(process.env.TOKEN)
